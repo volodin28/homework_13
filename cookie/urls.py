39b8cf767944db5e74a2402a4c38098ee9945d0a
Viacheslav Volodin <volodin28@gmail.com>
2023-06-20 12:45:52 +0300

    s3 bucket added as storages

--- cookie/urls.py
+++ cookie/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.cookies_list, name='cookies_list'),
+    path("", views.cookies_list, name="cookies_list"),
+    path("add/", views.add_cookie, name="add_cookie"),
 ]
