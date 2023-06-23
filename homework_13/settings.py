bd732a51a43364dbaeda9dcc5dac8b10651b8670
Viacheslav Volodin <volodin28@gmail.com>
2023-06-23 22:29:35 +0300

    fixes

--- homework_13/settings.py
+++ homework_13/settings.py
@@ -167,5 +167,5 @@ EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "3volodin28@gmail.com"
